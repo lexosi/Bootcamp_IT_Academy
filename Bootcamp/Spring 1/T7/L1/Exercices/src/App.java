a403f88186e53d59c9f1ef06d2876b08ff777c33
lexosi <lexosigamer@gmail.com>
2023-10-16 11:49:09 +0200

    fin

--- Bootcamp/Spring 1/T7/L1/Exercices/src/App.java
+++ Bootcamp/Spring 1/T7/L1/Exercices/src/App.java
@@ -7,12 +7,12 @@ public class App {
     @SuppressWarnings("deprecation")
     public static void main(String[] args) throws Exception {
         
-        final Worker DEFAULT_WORKER = new Worker("Pepe", "Hernandez", 20);
-        final OnlineWorker ONLINE_WORKER = new OnlineWorker("Raul", "Gamarra", 43);
-        final PresencialWorker PRESENCIAL_WORKER = new PresencialWorker("Adam", "Casas", 31);
+        Worker defaulWorker = new Worker("Pepe", "Hernandez", 20);
+        OnlineWorker onlineWorker = new OnlineWorker("Raul", "Gamarra", 43);
+        PresencialWorker presencialWorker = new PresencialWorker("Adam", "Casas", 31);
         
-        System.out.println("DefaultWorker Hours: " + DEFAULT_WORKER.calculateWage(10));
-        System.out.println("OnlineWorker Hours: " + ONLINE_WORKER.calculateWage(10));
-        System.out.println("PresencialWorker Hours: " + PRESENCIAL_WORKER.calculateWage(10));
+        System.out.println("DefaultWorker Hours: " + defaulWorker.calculateWage(10));
+        System.out.println("OnlineWorker Hours: " + onlineWorker.calculateWage(10));
+        System.out.println("PresencialWorker Hours: " + presencialWorker.calculateWage(10));
     }
 }
