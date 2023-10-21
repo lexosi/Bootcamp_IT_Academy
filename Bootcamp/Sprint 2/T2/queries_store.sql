da8f97e5a4f92dda649c79848b56c15dcd5ecb5a
lexosi <lexosigamer@gmail.com>
2023-10-21 16:48:57 +0200

    trans comments to english

--- Bootcamp/Sprint 2/T2/queries_store.sql
+++ Bootcamp/Sprint 2/T2/queries_store.sql
@@ -1,122 +1,122 @@
--- Llista el nom de tots els productes que hi ha en la taula producto.
+-- List the name of all the products in the producto table.
 SELECT nombre FROM producto;
 
--- Llista els noms i els preus de tots els productes de la taula producto.
+-- List the names and prices of all the products in the producto table.
 SELECT nombre, precio FROM producto;
 
--- Llista totes les columnes de la taula producto.
+-- List all the columns of the producto table.
 SELECT * FROM producto;
 
--- Llista el nom dels productes, el preu en euros i el preu en dòlars estatunidencs (USD).
-SELECT nombre AS "nom de producto", precio AS "euros", precio * 1.18 AS "dòlars" FROM producto;
+-- List the names of the products, the price in euros and the price in US dollars (USD).
+SELECT nombre AS "product name", precio AS "euros", precio * 1.18 AS "dollars" FROM producto;
 
--- Llista el nom dels productes, el preu en euros i el preu en dòlars estatunidencs (USD). Utilitza els següents àlies per a les columnes: nom de producto, euros, dòlars.
-SELECT nombre AS "nom de producto", precio AS "euros", precio * 1.18 AS "dòlars" FROM producto;
+-- List the names of the products, the price in euros and the price in US dollars (USD). Use the following aliases for the columns: product name, euros, dollars.
+SELECT nombre AS "product name", precio AS "euros", precio * 1.18 AS "dollars" FROM producto;
 
--- Llista els noms i els preus de tots els productes de la taula producto, convertint els noms a majúscula.
+-- List the names and prices of all the products in the producto table, converting the names to uppercase.
 SELECT UPPER(nombre), precio FROM producto;
 
--- Llista els noms i els preus de tots els productes de la taula producto, convertint els noms a minúscula.
+-- List the names and prices of all the products in the producto table, converting the names to lowercase.
 SELECT LOWER(nombre), precio FROM producto;
 
--- Llista el nom de tots els fabricants en una columna, i en una altra columna obtingui en majúscules els dos primers caràcters del nom del fabricant.
+-- List the name of all the manufacturers in one column, and in another column obtain in uppercase the first two characters of the manufacturer's name.
 SELECT nombre_fabricante, UPPER(SUBSTR(nombre_fabricante, 1, 2)) FROM fabricante;
 
--- Llista els noms i els preus de tots els productes de la taula producto, arrodonint el valor del preu.
+-- List the names and prices of all the products in the producto table, rounding the price value.
 SELECT nombre, ROUND(precio) FROM producto;
 
--- Llista els noms i els preus de tots els productes de la taula producto, truncant el valor del preu per a mostrar-lo sense cap xifra decimal.
+-- List the names and prices of all the products in the producto table, truncating the price value to show it without any decimal places.
 SELECT nombre, TRUNC(precio) FROM producto;
 
--- Llista el codi dels fabricants que tenen productes en la taula producto.
+-- List the code of the manufacturers that have products in the producto table.
 SELECT DISTINCT codigo_fabricante FROM producto;
 
--- Llista el codi dels fabricants que tenen productes en la taula producto, eliminant els codis que apareixen repetits.
+-- List the code of the manufacturers that have products in the producto table, eliminating the codes that appear repeated.
 SELECT DISTINCT codigo_fabricante FROM producto;
 
--- Llista els noms dels fabricants ordenats de manera ascendent.
+-- List the names of the manufacturers ordered in ascending order.
 SELECT nombre_fabricante FROM fabricante ORDER BY nombre_fabricante ASC;
 
--- Llista els noms dels fabricants ordenats de manera descendent.
+-- List the names of the manufacturers ordered in descending order.
 SELECT nombre_fabricante FROM fabricante ORDER BY nombre_fabricante DESC;
 
--- Llista els noms dels productes ordenats, en primer lloc, pel nom de manera ascendent i, en segon lloc, pel preu de manera descendent.
+-- List the names of the products ordered, first by name in ascending order and secondly by price in descending order.
 SELECT nombre, precio FROM producto ORDER BY nombre ASC, precio DESC;
 
--- Retorna una llista amb les 5 primeres files de la taula fabricante.
+-- Return a list with the first 5 rows of the fabricante table.
 SELECT * FROM fabricante LIMIT 5;
 
--- Retorna una llista amb 2 files a partir de la quarta fila de la taula fabricante. La quarta fila també s'ha d'incloure en la resposta.
+-- Return a list with 2 rows starting from the fourth row of the fabricante table. The fourth row must also be included in the response.
 SELECT * FROM fabricante LIMIT 2 OFFSET 3;
 
--- Llista el nom i el preu del producte més barat. (Utilitza solament les clàusules ORDER BY i LIMIT). NOTA: Aquí no podria usar MIN(preu), necessitaria GROUP BY.
+-- List the name and price of the cheapest product. (Use only the ORDER BY and LIMIT clauses). NOTE: Here I could not use MIN(price), I would need GROUP BY.
 SELECT nombre, precio FROM producto ORDER BY precio ASC LIMIT 1;
 
--- Llista el nom i el preu del producte més car. (Utilitza solament les clàusules ORDER BY i LIMIT). NOTA: Aquí no podria usar MAX(preu), necessitaria GROUP BY.
+-- List the name and price of the most expensive product. (Use only the ORDER BY and LIMIT clauses). NOTE: Here I could not use MAX(price), I would need GROUP BY.
 SELECT nombre, precio FROM producto ORDER BY precio DESC LIMIT 1;
 
--- Llista el nom de tots els productes del fabricant el codi de fabricant del qual és igual a 2.
+-- List the name of all the products of the manufacturer whose manufacturer code is equal to 2.
 SELECT nombre FROM producto WHERE codigo_fabricante = 2;
 
--- Retorna una llista amb el nom del producte, preu i nom de fabricant de tots els productes de la base de dades.
+-- Return a list with the name of the product, price and manufacturer name of all the products in the database.
 SELECT p.nombre, p.precio, f.nombre_fabricante FROM producto p, fabricante f WHERE p.codigo_fabricante = f.codigo_fabricante;
 
--- Retorna una llista amb el nom del producte, preu i nom de fabricant de tots els productes de la base de dades. Ordena el resultat pel nom del fabricant, per ordre alfabètic.
+-- Return a list with the name of the product, price and manufacturer name of all the products in the database. Order the result by the name of the manufacturer, in alphabetical order.
 SELECT p.nombre, p.precio, f.nombre_fabricante FROM producto p, fabricante f WHERE p.codigo_fabricante = f.codigo_fabricante ORDER BY f.nombre_fabricante ASC;
 
--- Retorna una llista amb el codi del producte, nom del producte, codi del fabricador i nom del fabricador, de tots els productes de la base de dades.
+-- Return a list with the code of the product, name of the product, manufacturer code and name of the manufacturer, of all the products in the database.
 SELECT p.codigo_producto, p.nombre, p.codigo_fabricante, f.nombre_fabricante FROM producto p, fabricante f WHERE p.codigo_fabricante = f.codigo_fabricante;
 
--- Retorna el nom del producte, el seu preu i el nom del seu fabricant, del producte més barat.
+-- Return the name of the product, its price and the name of its manufacturer, of the cheapest product.
 SELECT p.nombre, p.precio, f.nombre_fabricante FROM producto p, fabricante f WHERE p.codigo_fabricante = f.codigo_fabricante ORDER BY precio ASC LIMIT 1;
 
--- Retorna el nom del producte, el seu preu i el nom del seu fabricant, del producte més car.
+-- Return the name of the product, its price and the name of its manufacturer, of the most expensive product.
 SELECT p.nombre, p.precio, f.nombre_fabricante FROM producto p, fabricante f WHERE p.codigo_fabricante = f.codigo_fabricante ORDER BY precio DESC LIMIT 1;
 
--- Retorna una llista de tots els productes del fabricador Lenovo.
+-- Return a list of all the products of the Lenovo manufacturer.
 SELECT * FROM producto WHERE codigo_fabricante = (SELECT codigo_fabricante FROM fabricante WHERE nombre_fabricante = 'Lenovo');
 
--- Retorna una llista de tots els productes del fabricant Crucial que tinguin un preu major que 200 €.
+-- Return a list of all the products of the Crucial manufacturer that have a price greater than 200 €.
 SELECT * FROM producto WHERE codigo_fabricante = (SELECT codigo_fabricante FROM fabricante WHERE nombre_fabricante = 'Crucial') AND precio > 200;
 
--- Retorna un llistat amb tots els productes dels fabricants Asus, Hewlett-Packardy Seagate. Sense utilitzar l'operador IN.
+-- Return a list with all the products of the Asus, Hewlett-Packard and Seagate manufacturers. Without using the IN operator.
 SELECT p.nombre, p.precio FROM producto p, fabricante f WHERE p.codigo_fabricante = f.codigo_fabricante AND (f.nombre_fabricante = 'Asus' OR f.nombre_fabricante = 'Hewlett-Packard' OR f.nombre_fabricante = 'Seagate');
 
--- Retorna un llistat amb tots els productes dels fabricants Asus, Hewlett-Packardy Seagate. Fent servir l'operador IN.
+-- Return a list with all the products of the Asus, Hewlett-Packard and Seagate manufacturers. Using the IN operator.
 SELECT p.nombre, p.precio FROM producto p, fabricante f WHERE p.codigo_fabricante = f.codigo_fabricante AND f.nombre_fabricante IN ('Asus', 'Hewlett-Packard', 'Seagate');
 
--- Retorna un llistat amb el nom i el preu de tots els productes dels fabricants el nom dels quals acabi per la vocal e.
+-- Return a list with the name and price of all the products whose manufacturer name ends with the vowel e.
 SELECT p.nombre, p.precio FROM producto p, fabricante f WHERE p.codigo_fabricante = f.codigo_fabricante AND f.nombre_fabricante LIKE '%e';
 
--- Retorna un llistat amb el nom i el preu de tots els productes el nom de fabricant dels quals contingui el caràcter w en el seu nom.
+-- Return a list with the name and price of all the products whose manufacturer name contains the character w in its name.
 SELECT p.nombre, p.precio FROM producto p, fabricante f WHERE p.codigo_fabricante = f.codigo_fabricante AND f.nombre_fabricante LIKE '%w%';
 
--- Retorna un llistat amb el nom de producte, preu i nom de fabricant, de tots els productes que tinguin un preu major o igual a 180 €. Ordena el resultat, en primer lloc, pel preu (en ordre descendent) i, en segon lloc, pel nom (en ordre ascendent).
+-- Return a list with the name of the product, price and manufacturer name, of all the products that have a price greater than or equal to 180 €. Order the result, first by price (in descending order) and, secondly, by name (in ascending order).
 SELECT p.nombre, p.precio, f.nombre_fabricante FROM producto p, fabricante f WHERE p.codigo_fabricante = f.codigo_fabricante AND p.precio >= 180 ORDER BY p.precio DESC, p.nombre ASC;
 
--- Retorna un llistat amb el codi i el nom de fabricant, solament d'aquells fabricants que tenen productes associats en la base de dades.
+-- Return a list with the code and name of the manufacturer, only of those manufacturers that have associated products in the database.
 SELECT DISTINCT f.codigo_fabricante, f.nombre_fabricante FROM producto p, fabricante f WHERE p.codigo_fabricante = f.codigo_fabricante;
 
--- Retorna un llistat de tots els fabricants que existeixen en la base de dades, juntament amb els productes que té cadascun d'ells. El llistat haurà de mostrar també aquells fabricants que no tenen productes associats.
+-- Return a list of all the manufacturers that exist in the database, together with the products that each of them has. The list must also show those manufacturers that do not have associated products.
 SELECT f.nombre_fabricante, p.nombre, p.precio FROM fabricante f LEFT JOIN producto p ON f.codigo_fabricante = p.codigo_fabricante;
 
--- Retorna un llistat on només apareguin aquells fabricants que no tenen cap producte associat.
+-- Return a list where only those manufacturers that do not have any associated product appear.
 SELECT f.nombre_fabricante FROM fabricante f LEFT JOIN producto p ON f.codigo_fabricante = p.codigo_fabricante WHERE p.codigo_producto IS NULL;
 
--- Retorna tots els productes del fabricador Lenovo. (Sense utilitzar INNER JOIN).
+-- Return all the products of the Lenovo manufacturer. (Without using INNER JOIN).
 SELECT * FROM producto p, fabricante f WHERE p.codigo_fabricante = f.codigo_fabricante AND f.nombre_fabricante = 'Lenovo';
 
--- Retorna totes les dades dels productes que tenen el mateix preu que el producte més car del fabricant Lenovo. (Sense usar INNER JOIN).
+-- Return all the data of the products that have the same price as the most expensive product of the Lenovo manufacturer. (Without using INNER JOIN).
 SELECT * FROM producto WHERE precio = (SELECT MAX(precio) FROM producto WHERE codigo_fabricante = (SELECT codigo_fabricante FROM fabricante WHERE nombre_fabricante = 'Lenovo'));
 
--- Llista el nom del producte més car del fabricant Lenovo.
+-- List the name of the most expensive product of the Lenovo manufacturer.
 SELECT nombre FROM producto WHERE precio = (SELECT MAX(precio) FROM producto WHERE codigo_fabricante = (SELECT codigo_fabricante FROM fabricante WHERE nombre_fabricante = 'Lenovo'));
 
--- Llista el nom del producte més barat del fabricant Hewlett-Packard.
+-- List the name of the cheapest product of the Hewlett-Packard manufacturer.
 SELECT nombre FROM producto WHERE precio = (SELECT MIN(precio) FROM producto WHERE codigo_fabricante = (SELECT codigo_fabricante FROM fabricante WHERE nombre_fabricante = 'Hewlett-Packard'));
 
--- Retorna tots els productes de la base de dades que tenen un preu major o igual al producte més car del fabricant Lenovo.
+-- Return all the products of the database that have a price greater than or equal to the most expensive product of the Lenovo manufacturer.
 SELECT * FROM producto WHERE precio >= (SELECT MAX(precio) FROM producto WHERE codigo_fabricante = (SELECT codigo_fabricante FROM fabricante WHERE nombre_fabricante = 'Lenovo'));
 
--- Llesta tots els productes del fabricant Asus que tenen un preu superior al preu mitjà de tots els seus productes.
+-- List all the products of the Asus manufacturer that have a price higher than the average price of all its products.
 SELECT nombre, precio FROM producto WHERE codigo_fabricante = (SELECT codigo_fabricante FROM fabricante WHERE nombre_fabricante = 'Asus') AND precio > (SELECT AVG(precio) FROM producto WHERE codigo_fabricante = (SELECT codigo_fabricante FROM fabricante WHERE nombre_fabricante = 'Asus'));
--- Bootcamp/Sprint 2/T2/queries_university.sql
+++ Bootcamp/Sprint 2/T2/queries_university.sql
@@ -1,38 +1,38 @@
--- Retorna un llistat amb el primer cognom, segon cognom i el nom de tots els/les alumnes. El llistat haurà d'estar ordenat alfabèticament de menor a major pel primer cognom, segon cognom i nom.
+-- Returns a list with the first surname, second surname and name of all students. The list must be ordered alphabetically from lowest to highest by first surname, second surname and name.
 SELECT primer_cognom, segon_cognom, nom
 FROM persona
 WHERE tipo = 'alumno'
 ORDER BY primer_cognom, segon_cognom, nom;
 
--- Esbrina el nom i els dos cognoms dels alumnes que no han donat d'alta el seu número de telèfon en la base de dades.
+-- Finds the name and both surnames of the students who have not registered their phone number in the database.
 SELECT nom, primer_cognom, segon_cognom
 FROM persona
 WHERE tipo = 'alumno' AND telefon IS NULL;
 
--- Retorna el llistat dels alumnes que van néixer en 1999.
+-- Returns the list of students who were born in 1999.
 SELECT *
 FROM persona
 WHERE tipo = 'alumno' AND YEAR(data_naixement) = 1999;
 
--- Retorna el llistat de professors/es que no han donat d'alta el seu número de telèfon en la base de dades i a més el seu NIF acaba en K.
+-- Returns the list of professors who have not registered their phone number in the database and whose NIF ends in K.
 SELECT *
 FROM persona p
 INNER JOIN profesor pr ON p.id = pr.id_profesor
 WHERE p.telefon IS NULL AND p.nif LIKE '%K';
 
--- Retorna el llistat de les assignatures que s'imparteixen en el primer quadrimestre, en el tercer curs del grau que té l'identificador 7.
+-- Returns the list of subjects taught in the first semester, in the third year of the degree with identifier 7.
 SELECT *
 FROM asignatura
 WHERE cuatrimestre = 1 AND curso = 3 AND id_grado = 7;
 
--- Retorna un llistat dels professors/es juntament amb el nom del departament al qual estan vinculats. El llistat ha de retornar quatre columnes, primer cognom, segon cognom, nom i nom del departament. El resultat estarà ordenat alfabèticament de menor a major pels cognoms i el nom.
+-- Returns a list of professors along with the name of the department they are associated with. The list must return four columns, first surname, second surname, name and department name. The result will be ordered alphabetically from lowest to highest by surnames and name.
 SELECT p.primer_cognom, p.segon_cognom, p.nom, d.nom AS nom_departament
 FROM persona p
 INNER JOIN profesor pr ON p.id = pr.id_profesor
 INNER JOIN departamento d ON pr.id_departamento = d.id
 ORDER BY p.primer_cognom, p.segon_cognom, p.nom;
 
--- Retorna un llistat amb el nom de les assignatures, any d'inici i any de fi del curs escolar de l'alumne/a amb NIF 26902806M.
+-- Returns a list with the name of the subjects, year of start and year of end of the school year of the student with NIF 26902806M.
 SELECT a.nombre, ce.anyo_inicio, ce.anyo_fin
 FROM persona p
 INNER JOIN alumno_se_matricula_asignatura am ON p.id = am.id_alumno
@@ -40,7 +40,7 @@ INNER JOIN asignatura a ON am.id_asignatura = a.id
 INNER JOIN curso_escolar ce ON am.id_curso_escolar = ce.id
 WHERE p.nif = '26902806M';
 
--- Retorna un llistat amb el nom de tots els departaments que tenen professors/es que imparteixen alguna assignatura en el Grau en Enginyeria Informàtica (Pla 2015).
+-- Returns a list with the names of all departments that have professors who teach some subject in the Degree in Computer Engineering (Plan 2015).
 SELECT DISTINCT d.nom
 FROM departamento d
 INNER JOIN profesor pr ON d.id = pr.id_departamento
@@ -48,46 +48,46 @@ INNER JOIN asignatura a ON pr.id_profesor = a.id_profesor
 INNER JOIN grado g ON a.id_grado = g.id
 WHERE g.nombre = 'Enginyeria Informàtica' AND g.pla = 2015;
 
--- Retorna un llistat amb tots els alumnes que s'han matriculat en alguna assignatura durant el curs escolar 2018/2019.
+-- Returns a list of all students who have enrolled in some subject during the 2018/2019 school year.
 SELECT p.*
 FROM persona p
 INNER JOIN alumno_se_matricula_asignatura am ON p.id = am.id_alumno
 INNER JOIN curso_escolar ce ON am.id_curso_escolar = ce.id
 WHERE YEAR(ce.anyo_inicio) = 2018 AND YEAR(ce.anyo_fin) = 2019;
 
--- Retorna un llistat amb els noms de tots els professors/es i els departaments que tenen vinculats. El llistat també ha de mostrar aquells professors/es que no tenen cap departament associat. El llistat ha de retornar quatre columnes, nom del departament, primer cognom, segon cognom i nom del professor/a. El resultat estarà ordenat alfabèticament de menor a major pel nom del departament, cognoms i el nom.
+-- Returns a list with the names of all professors and the departments they are associated with. The list must also show those professors who do not have any associated department. The list must return four columns, department name, first surname, second surname and name of the professor. The result will be ordered alphabetically from lowest to highest by department name, surnames and name.
 SELECT d.nom AS nom_departament, p.primer_cognom, p.segon_cognom, p.nom
 FROM persona p
 LEFT JOIN profesor pr ON p.id = pr.id_profesor
 LEFT JOIN departamento d ON pr.id_departamento = d.id
 ORDER BY nom_departament, p.primer_cognom, p.segon_cognom, p.nom;
 
--- Retorna un llistat amb els professors/es que no estan associats a un departament.
+-- Returns a list with the professors who are not associated with a department.
 SELECT p.*
 FROM persona p
 LEFT JOIN profesor pr ON p.id = pr.id_profesor
 WHERE pr.id_departamento IS NULL;
 
--- Retorna un llistat amb els departaments que no tenen professors/es associats.
+-- Returns a list with the departments that do not have associated professors.
 SELECT d.*
 FROM departamento d
 LEFT JOIN profesor pr ON d.id = pr.id_departamento
 WHERE pr.id_profesor IS NULL;
 
--- Retorna un llistat amb els professors/es que no imparteixen cap assignatura.
+-- Returns a list with the professors who do not teach any subject.
 SELECT p.*
 FROM persona p
 LEFT JOIN profesor pr ON p.id = pr.id_profesor
 LEFT JOIN asignatura a ON pr.id_profesor = a.id_profesor
 WHERE a.id IS NULL;
 
--- Retorna un llistat amb les assignatures que no tenen un professor/a assignat.
+-- Returns a list with the subjects that do not have an assigned professor.
 SELECT a.*
 FROM asignatura a
 LEFT JOIN profesor pr ON a.id_profesor = pr.id_profesor
 WHERE pr.id_profesor IS NULL;
 
--- Retorna un llistat amb tots els departaments que no han impartit assignatures en cap curs escolar.
+-- Returns a list with all departments that have not taught any subject in any school year.
 SELECT d.*
 FROM departamento d
 LEFT JOIN profesor pr ON d.id = pr.id_departamento
@@ -96,38 +96,38 @@ LEFT JOIN alumno_se_matricula_asignatura am ON a.id = am.id_asignatura
 LEFT JOIN curso_escolar ce ON am.id_curso_escolar = ce.id
 WHERE ce.id IS NULL;
 
--- Retorna el nombre total d'alumnes que hi ha.
+-- Returns the total number of students.
 SELECT COUNT(*) AS total_alumnos
 FROM persona
 WHERE tipo = 'alumno';
 
--- Calcula quants alumnes van néixer en 1999.
+-- Calculates how many students were born in 1999.
 SELECT COUNT(*) AS alumnos_1999
 FROM persona
 WHERE tipo = 'alumno' AND YEAR(data_naixement) = 1999;
 
--- Calcula quants professors/es hi ha en cada departament. El resultat només ha de mostrar dues columnes, una amb el nom del departament i una altra amb el nombre de professors/es que hi ha en aquest departament. El resultat només ha d'incloure els departaments que tenen professors/es associats i haurà d'estar ordenat de major a menor pel nombre de professors/es.
+-- Calculates how many professors there are in each department. The result must only show two columns, one with the name of the department and another with the number of professors in that department. The result must only include the departments that have associated professors and must be ordered from highest to lowest by the number of professors.
 SELECT d.nom AS nom_departament, COUNT(*) AS num_profesores
 FROM departamento d
 INNER JOIN profesor p ON d.id = p.id_departamento
 GROUP BY d.id
 ORDER BY num_profesores DESC;
 
--- Retorna un llistat amb tots els departaments i el nombre de professors/es que hi ha en cadascun d'ells. Tingui en compte que poden existir departaments que no tenen professors/es associats. Aquests departaments també han d'aparèixer en el llistat.
+-- Returns a list with all departments and the number of professors in each one. Note that there may be departments that do not have associated professors. These departments must also appear in the list.
 SELECT d.nom AS nom_departament, COUNT(p.id_profesor) AS num_profesores
 FROM departamento d
 LEFT JOIN profesor p ON d.id = p.id_departamento
 GROUP BY d.id
 ORDER BY num_profesores DESC;
 
--- Retorna un llistat amb el nom de tots els graus existents en la base de dades i el nombre d'assignatures que té cadascun. Tingues en compte que poden existir graus que no tenen assignatures associades. Aquests graus també han d'aparèixer en el llistat. El resultat haurà d'estar ordenat de major a menor pel nombre d'assignatures.
+-- Returns a list with the name of all degrees in the database and the number of subjects each one has. Note that there may be degrees that do not have associated subjects. These degrees must also appear in the list. The result must be ordered from highest to lowest by the number of subjects.
 SELECT g.nombre, COUNT(a.id) AS num_assignatures
 FROM grado g
 LEFT JOIN asignatura a ON g.id = a.id_grado
 GROUP BY g.id
 ORDER BY num_assignatures DESC;
 
--- Retorna un llistat amb el nom de tots els graus existents en la base de dades i el nombre d'assignatures que té cadascun, dels graus que tinguin més de 40 assignatures associades.
+-- Returns a list with the name of all degrees in the database and the number of subjects each one has, for degrees that have more than 40 associated subjects.
 SELECT g.nombre, COUNT(a.id) AS num_assignatures
 FROM grado g
 LEFT JOIN asignatura a ON g.id = a.id_grado
@@ -135,19 +135,19 @@ GROUP BY g.id
 HAVING num_assignatures > 40
 ORDER BY num_assignatures DESC;
 
--- Retorna un llistat que mostri el nom dels graus i la suma del nombre total de crèdits que hi ha per a cada tipus d'assignatura. El resultat ha de tenir tres columnes: nom del grau, tipus d'assignatura i la suma dels crèdits de totes les assignatures que hi ha d'aquest tipus.
+-- Returns a list that shows the name of the degrees and the sum of the total number of credits for each type of subject. The result must have three columns: name of the degree, type of subject and the sum of the credits of all subjects of that type.
 SELECT g.nombre, a.tipo, SUM(a.creditos) AS total_creditos
 FROM grado g
 LEFT JOIN asignatura a ON g.id = a.id_grado
 GROUP BY g.id, a.tipo;
 
--- Retorna un llistat que mostri quants alumnes s'han matriculat d'alguna assignatura en cadascun dels cursos escolars. El resultat haurà de mostrar dues columnes, una columna amb l'any d'inici del curs escolar i una altra amb el nombre d'alumnes matriculats.
+-- Returns a list that shows how many students have enrolled in some subject in each school year. The result must show two columns, one with the year of start of the school year and another with the number of enrolled students.
 SELECT YEAR(ce.anyo_inicio) AS anyo, COUNT(DISTINCT am.id_alumno) AS num_alumnos
 FROM curso_escolar ce
 LEFT JOIN alumno_se_matricula_asignatura am ON ce.id = am.id_curso_escolar
 GROUP BY anyo;
 
--- Retorna un llistat amb el nombre d'assignatures que imparteix cada professor/a. El llistat ha de tenir en compte aquells professors/es que no imparteixen cap assignatura. El resultat mostrarà cinc columnes: id, nom, primer cognom, segon cognom i nombre d'assignatures. El resultat estarà ordenat de major a menor pel nombre d'assignatures.
+-- Returns a list with the number of subjects taught by each professor. The list must take into account those professors who do not teach any subject. The result will show five columns: id, name, first surname, second surname and number of subjects. The result will be ordered from highest to lowest by the number of subjects.
 SELECT p.id_profesor, p.nom, p.primer_cognom, p.segon_cognom, COUNT(a.id) AS num_assignatures
 FROM persona p
 LEFT JOIN profesor pr ON p.id = pr.id_profesor
@@ -155,14 +155,14 @@ LEFT JOIN asignatura a ON pr.id_profesor = a.id_profesor
 GROUP BY p.id
 ORDER BY num_assignatures DESC;
 
--- Retorna totes les dades de l'alumne/a més jove.
+-- Returns all data from the youngest student.
 SELECT *
 FROM persona
 WHERE tipo = 'alumno'
 ORDER BY data_naixement DESC
 LIMIT 1;
 
--- Retorna un llistat amb els professors/es que tenen un departament associat i que no imparteixen cap assignatura.
+-- Returns a list with the professors who have an associated department and who do not teach any subject.
 SELECT p.*
 FROM persona p
 INNER JOIN profesor pr ON p.id = pr.id_profesor
