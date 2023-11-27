be550ac5f4f922909213d7d4f13e7590af67e91f
lexosi <iamlexosi@gmail.com>
2023-11-27 11:07:45 +0100

    Fix bug in login functionality

--- Bootcamp/Sprint 3/T2/app/src/main/java/t2/StockMarket.java
+++ Bootcamp/Sprint 3/T2/app/src/main/java/t2/StockMarket.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-import java.util.List;
+import java.util.List; 
 
 public class StockMarket {
      private List<Observer> observers = new ArrayList<>();
---
+++ Bootcamp/Sprint 4/T1/S04T01N01MorenoFortAdolfo/src/test/java/cat/itacademy/barcelonactiva/MorenoFort/Adolfo/s04/t01/n01/S04T01N01MorenoFortAdolfo/S04T01N01MorenoFortAdolfoApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.MorenoFort.Adolfo.s04.t01.n01.S04T01N01MorenoFortAdolfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S04T01N01MorenoFortAdolfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
