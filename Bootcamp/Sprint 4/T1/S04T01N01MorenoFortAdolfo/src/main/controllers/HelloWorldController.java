289ce8d538b65d99d6f8fe1ad1c063c534202f87
lexosi <iamlexosi@gmail.com>
2023-11-27 11:47:12 +0100

    Fix bug in login functionality

---
+++ Bootcamp/Sprint 4/T1/S04T01N01MorenoFortAdolfo/src/main/controllers/HelloWorldController.java
@@ -0,0 +1,22 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    String nom = "Adolfo";
+    
+    @GetMapping("/HelloWorld")
+    public String saluda(@RequestParam(value = "nom", defaultValue = "UNKNOWN") String nom) {
+        return "Hola, " + nom + ". Estàs executant un projecte Maven";
+    }
+
+    @GetMapping({"/HelloWorld2", "/HelloWorld2/{nom}"})
+    public String saluda2(@PathVariable(required = false) String nom) {
+        if (nom == null) {
+            nom = "UNKNOWN";
+        }
+        return "Hola, " + nom + ". Estàs executant un projecte Maven";
+    }
+}
