a23adf5dee0a7d033d96ede13a36e8c13ddf7a43
lexosi <iamlexosi@gmail.com>
2024-01-08 17:11:04 +0100

    Update package imports for FrutaController, FrutaRepository, and FrutaService

--- Bootcamp/Sprint 4/T2/S04T02N01MorenoFortAdolfo/src/main/java/cat/itacademy/barcelonactiva/MorenoFort/Adolfo/s04/t02/n01/S04T02N01MorenoFortAdolfo/controllers/FrutaController.java
+++ Bootcamp/Sprint 4/T2/S04T02N01MorenoFortAdolfo/src/main/java/cat/itacademy/barcelonactiva/MorenoFort/Adolfo/s04/t02/n01/S04T02N01MorenoFortAdolfo/controllers/FrutaController.java
@@ -2,8 +2,8 @@ package cat.itacademy.barcelonactiva.MorenoFort.Adolfo.s04.t02.n01.S04T02N01More
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import cat.itacademy.barcelonactiva.MorenoFort.Adolfo.s04.t02.n01.model.domain.Fruta;
-import cat.itacademy.barcelonactiva.MorenoFort.Adolfo.s04.t02.n01.model.services.FrutaService;
+import cat.itacademy.barcelonactiva.MorenoFort.Adolfo.s04.t02.n01.S04T02N01MorenoFortAdolfo.model.domain.Fruta;
+import cat.itacademy.barcelonactiva.MorenoFort.Adolfo.s04.t02.n01.S04T02N01MorenoFortAdolfo.model.services.FrutaService;
 
 @RestController
 @RequestMapping("/fruta")
--- Bootcamp/Sprint 4/T2/S04T02N01MorenoFortAdolfo/src/main/java/cat/itacademy/barcelonactiva/MorenoFort/Adolfo/s04/t02/n01/S04T02N01MorenoFortAdolfo/model/services/FrutaService.java
+++ Bootcamp/Sprint 4/T2/S04T02N01MorenoFortAdolfo/src/main/java/cat/itacademy/barcelonactiva/MorenoFort/Adolfo/s04/t02/n01/S04T02N01MorenoFortAdolfo/model/services/FrutaService.java
@@ -1,8 +1,8 @@
 package cat.itacademy.barcelonactiva.MorenoFort.Adolfo.s04.t02.n01.S04T02N01MorenoFortAdolfo.model.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import cat.itacademy.barcelonactiva.MorenoFort.Adolfo.s04.t02.n01.model.domain.Fruta;
-import cat.itacademy.barcelonactiva.MorenoFort.Adolfo.s04.t02.n01.model.repository.FrutaRepository;
+import cat.itacademy.barcelonactiva.MorenoFort.Adolfo.s04.t02.n01.S04T02N01MorenoFortAdolfo.model.domain.Fruta;
+import cat.itacademy.barcelonactiva.MorenoFort.Adolfo.s04.t02.n01.S04T02N01MorenoFortAdolfo.model.repository.FrutaRepository;
 
 @Service
 public class FrutaService {
