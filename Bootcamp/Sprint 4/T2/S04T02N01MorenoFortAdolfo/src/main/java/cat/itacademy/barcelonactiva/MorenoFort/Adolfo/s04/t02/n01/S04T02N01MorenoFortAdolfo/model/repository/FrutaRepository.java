5fd92d8fcd245941c7e2e290bd5fa82d3123f046
lexosi <iamlexosi@gmail.com>
2024-01-09 11:32:06 +0100

    Update sourceCompatibility and targetCompatibility to 1.17 Delete FrutaController, Fruta, FrutaRepository, and FrutaService

--- Bootcamp/Sprint 4/T2/S04T02N01MorenoFortAdolfo/src/main/java/cat/itacademy/barcelonactiva/MorenoFort/Adolfo/s04/t02/n01/S04T02N01MorenoFortAdolfo/model/repository/FrutaRepository.java
+++
@@ -1,7 +0,0 @@
-package cat.itacademy.barcelonactiva.MorenoFort.Adolfo.s04.t02.n01.S04T02N01MorenoFortAdolfo.model.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import cat.itacademy.barcelonactiva.MorenoFort.Adolfo.s04.t02.n01.S04T02N01MorenoFortAdolfo.model.domain.Fruta;
-
-public interface FrutaRepository extends JpaRepository<Fruta, Integer> {
-}
